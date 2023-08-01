362e50e2c6596f9d8bd1ba8a63d96b0bf202c18f
wintercodeNKJJ <kenfackjordanjuunior@gmail.com>
2023-08-01 05:20:08 +0100

    to do

--- src/components/mainItem/modifier.jsx
+++ src/components/mainItem/modifier.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {BsStar} from 'react-icons/bs'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsSun } from 'react-icons/bs'
+import { RxCross1 } from 'react-icons/rx'
 
 const Modifier = () => {
   return (
@@ -9,6 +12,25 @@ const Modifier = () => {
           <input type='checkbox' className='ckbox'/>
           <input type="text" placeholder='Title...' name='title' className='modifier-title'/>
           <i><BsStar size={20}/></i>
+        </div>
+
+        <div className='entry'>
+          <input type='checkbox' className='ckbox'/>
+          <input type="text" placeholder='subitem...' name='title' className='modifier-title'/>
+        </div>
+
+        <div className='add-entry'>
+          <i><AiOutlinePlus size={20}/></i>
+          <input type="text" placeholder='Next Step' name='title' className='modifier-title'/>
+        </div>
+      </div>
+      <div className='modifier-middle'>
+        <div className='middle-option'>
+          <div className='op-title'>
+            <i><BsSun size={20}/></i>
+            <h2>Add to My Day</h2>
+          </div>
+          <i><RxCross1 size={20}/></i>
         </div>
       </div>
     </form>
